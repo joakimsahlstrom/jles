ff22f7f0c5bd26e2bf57ca1cba68c50528bb55ed
joakim.sahlstrom <joakim.sahlstrom@home.se>
2013-10-25 22:27:52 +0200

    Configurer now stores less state

--- src/main/java/se/jsa/jles/EventStoreConfigurer.java
+++ src/main/java/se/jsa/jles/EventStoreConfigurer.java
@@ -18,20 +18,20 @@ import se.jsa.jles.internal.eventdefinitions.MemoryBasedEventDefinitions;
 import se.jsa.jles.internal.eventdefinitions.PersistingEventDefinitions;
 import se.jsa.jles.internal.file.FlippingEntryFile;
 
+/**
+ * Used for creating fully initialized {@link EventStore} instances
+ * @author joakim Joakim Sahlström
+ */
 public class EventStoreConfigurer {
 	private final FileChannelFactory fileChannelFactory;
-	private final EventFile eventFile;
-	private final FlippingEntryFile fallbackIndexFile;
 	private final Set<Class<?>> indexedEventTypes = new HashSet<Class<?>>();
-	private EventDefinitions eventDefinitions;
+	private boolean useFileBasedEventDefinitions;
 
 	private final List<String> files = new ArrayList<String>();
 
 	public EventStoreConfigurer(FileChannelFactory fileChannelFactory) {
 		this.fileChannelFactory = fileChannelFactory;
-		this.eventFile = new EventFile(createEntryFile("events.ef", fileChannelFactory));
-		this.fallbackIndexFile = createEntryFile("events.if", fileChannelFactory);
-		this.eventDefinitions = new MappingEventDefinitions(new PersistingEventDefinitions(new EventDefinitionFile(createEntryFile("events.def", fileChannelFactory))));
+		this.useFileBasedEventDefinitions = true;
 	}
 
 	public EventStoreConfigurer addIndexing(Class<?> eventType) {
@@ -40,25 +40,39 @@ public class EventStoreConfigurer {
 	}
 
 	public EventStoreConfigurer testableEventDefinitions() {
-		this.eventDefinitions = new MappingEventDefinitions(new MemoryBasedEventDefinitions());
+		this.useFileBasedEventDefinitions = false;
 		return this;
 	}
 
 	public EventStore configure() {
+		FlippingEntryFile eventTypeIndexFile = createEntryFile("events.if", fileChannelFactory);
+		EventFile eventFile = new EventFile(createEntryFile("events.ef", fileChannelFactory));
+
+		EventDefinitions eventDefinitions = createEventDefinitions();
 		eventDefinitions.init();
-		Indexing indexing = createIndexing(eventDefinitions);
+
+		Indexing indexing = createIndexing(eventTypeIndexFile, eventDefinitions);
+
 		EventStore result = new EventStore(eventFile, indexing, eventDefinitions);
 		return result;
 	}
 
-	private Indexing createIndexing(EventDefinitions eventDefinitions) {
+	private EventDefinitions createEventDefinitions() {
+		if (useFileBasedEventDefinitions) {
+			return new MappingEventDefinitions(new PersistingEventDefinitions(new EventDefinitionFile(createEntryFile("events.def", fileChannelFactory))));
+		} else {
+			return new MappingEventDefinitions(new MemoryBasedEventDefinitions());
+		}
+	}
+
+	private Indexing createIndexing(FlippingEntryFile eventTypeIndexFile, EventDefinitions eventDefinitions) {
 		HashMap<Long, EventIndex> eventIndicies = new HashMap<Long, EventIndex>();
 		for (Class<?> indexedEventType : indexedEventTypes) {
 			for (Long eventTypeId : eventDefinitions.getEventTypeIds(indexedEventType)) {
 				eventIndicies.put(eventTypeId, new EventIndex(createEntryFile("events_" + eventTypeId + ".if", fileChannelFactory), eventTypeId));
 			}
 		}
-		return new Indexing(fallbackIndexFile, eventIndicies, Collections.<EventFieldIndex.EventFieldId, EventFieldIndex>emptyMap());
+		return new Indexing(eventTypeIndexFile, eventIndicies, Collections.<EventFieldIndex.EventFieldId, EventFieldIndex>emptyMap());
 	}
 
 	private FlippingEntryFile createEntryFile(String fileName, FileChannelFactory fileChannelFactory) {
